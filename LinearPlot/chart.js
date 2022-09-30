bcd18a6091eea8564ee0c83ada79e1b511b26341
Abdulkhak <a.abdulkhak@gmail.com>
2022-09-30 14:48:15 +0600

    updated

--- LinearPlot/chart.js
+++ LinearPlot/chart.js
@@ -1,10 +1,9 @@
 async function buildPlot() {
-    console.log("Hello world");
     const data = await d3.json("my_weather_data.json");
-    //console.table(data);
     const dateParser = d3.timeParse("%Y-%m-%d");
     const yAccessor = (d) => d.temperatureMin;
     const xAccessor = (d) => dateParser(d.date);
+    const y2 = (d) => d.temperatureHigh;
     // Функции для инкапсуляции доступа к колонкам набора данных
 
     var dimension = {
@@ -36,9 +35,8 @@ async function buildPlot() {
         .domain(d3.extent(data,xAccessor))
         .range([0,dimension.boundedWidth]);
 
-    const high = (d) => d.temperatureHigh;
-    const highScaler = d3.scaleLinear()
-        .domain(d3.extent(data,high))
+    const y2Scaler = d3.scaleLinear()
+        .domain(d3.extent(data, y2))
         .range([dimension.boundedHeight,0]);
 
     var lineGenerator = d3.line()
@@ -47,29 +45,25 @@ async function buildPlot() {
 
     bounded.append("path")
         .attr("d",lineGenerator(data))
-        // .attr("fill","none")
         .attr("stroke","red")
 
     lineGenerator = d3.line()
         .x(d => xScaler(xAccessor(d)))
-        .y(d => highScaler(high(d)));
+        .y(d => y2Scaler(y2(d)));
 
     bounded.append("path")
         .attr("d",lineGenerator(data))
         .attr("stroke","blue")
 
+    var xaxis = d3.axisBottom().scale(xScaler);
+    var yaxis = d3.axisRight().scale(yScaler);
 
-    var scale = d3.scaleLinear()
-        .domain([d3.min(yAccessor(d)), d3.max(high(d))])
-        .range([0,dimension.boundedWidth]);
-
-    // Add scales to axis
-    var x_axis = d3.axisBottom()
-        .scale(scale);
-
-    //Append group and insert axis
-    svg.append("g")
-        .call(x_axis);
+    svg.append('g')
+        .attr("transform", `translate(0, ${dimension.boundedHeight})`)
+        .call(xaxis);
+    svg.append('g')
+        .attr("transform", `translate(${dimension.margin.left}px, 0)`)
+        .call(yaxis)
 }
 
 buildPlot();
